9f55e1b70c52523a1b1a1cc6d941b03f0e3d8181
unknown <muazamsport@gmail.com>
2020-11-16 23:52:01 +0800

    push

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="<?php echo "images/". $post_image ?>" alt="">
                 <hr>
                 <p><?php echo $post_content ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
@@ -48,10 +48,6 @@
             }
 
             ?>
-
-
-
-
 
         </div>
 
